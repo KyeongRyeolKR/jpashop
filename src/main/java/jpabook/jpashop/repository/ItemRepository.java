667438af29fa10e307e1e9b2332667fbbf7b2eb4
김경렬 <rlarudfuf802@naver.com>
2023-10-25 13:56:31 +0900

    상품 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if(item.getId() == null) {
+            em.persist(item); // 신규 등록
+        } else {
+            em.merge(item); // 이미 존재하는 상품 업데이트
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
